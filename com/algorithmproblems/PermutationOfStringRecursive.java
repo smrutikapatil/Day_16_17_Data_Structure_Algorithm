f27b8a6e70f92e6bc7f6b8cd549a9cec0377a333
smrutikapatil <smruti.patil17@gmail.com>
2022-03-04 21:14:25 +0530

    Added- Programme to Permutation Of String recursion

---
+++ com/algorithmproblems/PermutationOfStringRecursive.java
@@ -0,0 +1,38 @@
+package com.algorithmproblems;
+
+import java.util.Scanner;
+
+public class PermutationOfStringRecursive {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string which you want to find permutation");
+		String string = sc.nextLine();
+		int number;
+		number = string.length();
+		PermutationOfStringRecursive.permutationOfStringRecursive(string, 0, number - 1);
+
+		sc.close();
+	}
+
+	public static void permutationOfStringRecursive(String string, int lower, int higher) {
+		StringBuilder strB = new StringBuilder(string);
+		int j;
+		if (lower == higher) {
+
+			System.out.print(strB + " ");
+		} else {
+			for (j = lower; j <= higher; j++) {
+				char l = strB.charAt(lower), r = strB.charAt(j);
+				strB.setCharAt(lower, r);
+				strB.setCharAt(j, l);
+				String newval = strB.toString();
+				permutationOfStringRecursive(newval, lower + 1, higher);
+				l = strB.charAt(lower);
+				r = strB.charAt(j);
+				strB.setCharAt(lower, r);
+				strB.setCharAt(j, l);
+			}
+		}
+	}
+}
