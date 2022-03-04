86ca2bafa8950d70ba2c5045705b63ed4edd2164
smrutikapatil <smruti.patil17@gmail.com>
2022-03-04 21:09:54 +0530

    Added- Programme to Permutation Of String Iterative

---
+++ com/algorithmproblems/PermutationOfStringIterative.java
@@ -0,0 +1,43 @@
+package com.algorithmproblems;
+
+import java.util.Scanner;
+
+public class  PermutationOfStringIterative {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the string which you want to find the permutation:");
+		String string = sc.nextLine();
+		PermutationOfStringIterative.permutationOfStringIterative(string);
+		sc.close();
+	}
+
+	public static void permutationOfStringIterative(String string) {
+		int k = string.length();
+		int[] p = new int[k];
+		int i;
+		for (i = 0; i < k; i++) {
+			p[i] = 0;
+		}
+		System.out.print(string);
+		i = 1;
+		int j = 0;
+		while (i < k) {
+
+			if (p[i] < i) {
+
+				j = (i % 2) * p[i];
+				StringBuilder strB = new StringBuilder(string);
+				char l = strB.charAt(i), r = strB.charAt(j);
+				strB.setCharAt(i, r);
+				strB.setCharAt(j, l);
+				System.out.print(" " + strB);
+				p[i]++;
+				i = 1;
+			} else {
+				p[i] = 0;
+				i++;
+			}
+		}
+	}
+}
